80d23693ac8f7d5cc84210e2fa9fe44b526d23d6
MarauderPixie <tobias.anton.85@gmail.com>
2016-02-13 10:13:07 +0100

    ygyro - you only get YOLD right once

--- dawn.R
+++ dawn.R
@@ -12,5 +12,12 @@ ddate <- function(){
   
   holydays <- data.frame("5th"  = c("Mungday", "Mojoday", "Syaday", "Zaraday", "Maladay"),
                          "50th" = c("Chaoflux", "Discoflux", "Confuflux", "Bureflux", "Afflux"))
+  
+  today <- as.list(unlist(strsplit(system("date",T)," ")))
+  names(today)<-c("weekday","month","day","time","timezone","year")
+  
+  YOLD <- as.numeric(today$year) + 1166
   return(*object*)
-}
+  }
+
+system(command = 'ddate', intern = T)
