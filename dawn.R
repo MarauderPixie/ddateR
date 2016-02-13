efaaf8dfb9854e406e9c1701f9b0895fab35e1f4
MarauderPixie <tobias.anton.85@gmail.com>
2016-02-13 09:18:13 +0100

    don't judge, pls

---
+++ dawn.R
@@ -0,0 +1,16 @@
+days <- factor(c("Sweetmorn", "Boomtime", "Pungenday", "Prickle-Prickle", "Setting Orange"))
+seasons <- factor(c("Chaos", "Discord", "Confusion", "Bureaucracy", "The Aftermath"))
+
+holydays <- data.frame("5th"  = c("Mungday", "Mojoday", "Syaday", "Zaraday", "Maladay"),
+                       "50th" = c("Chaoflux", "Discoflux", "Confuflux", "Bureflux", "Afflux"))
+## ...?
+holydays.m <- matrix(holydays)
+
+ddate <- function(){
+  days     <- factor(c("Sweetmorn", "Boomtime", "Pungenday", "Prickle-Prickle", "Setting Orange"))
+  seasons  <- factor(c("Chaos", "Discord", "Confusion", "Bureaucracy", "The Aftermath"))
+  
+  holydays <- data.frame("5th"  = c("Mungday", "Mojoday", "Syaday", "Zaraday", "Maladay"),
+                         "50th" = c("Chaoflux", "Discoflux", "Confuflux", "Bureflux", "Afflux"))
+  return(*object*)
+}
