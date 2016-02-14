98da51f8a5e7675b87b5a2d96f52951c04c34669
MarauderPixie <tobias.anton.85@gmail.com>
2016-02-14 21:37:53 +0100

    add lots of spaces

--- dawn.R
+++ dawn.R
@@ -1,6 +1,6 @@
 ## ...?
 
-ddate <- function(){
+ddate <- function() {
   # days have names and those names want to be recognized. also seasons. and holydays.
   days     <- factor(c(1:5), labels = c("Sweetmorn", "Boomtime", "Pungenday", "Prickle-Prickle", "Setting Orange"))
   seasons  <- factor(c(1:5), labels = c("Chaos", "Discord", "Confusion", "Bureaucracy", "The Aftermath"))
@@ -27,27 +27,27 @@ ddate <- function(){
   nod <- as.numeric(Sys.Date() - as.Date("2016-01-01") + 1)
   # nod <- lubridate::yday(Sys.Date())
   
-  season <- if(nod <=  73){"Chaos"} else 
-            if(nod >=  74 && nod <= 146){"Discord"} else
-            if(nod >= 147 && nod <= 219){"Confusion"} else
-            if(nod >= 220 && nod <= 292){"Bureaucracy"} else
-            if(nod >= 293 && nod <= 365){"The Aftermath"}
+  season <- if (nod <=  73) {"Chaos"} else 
+            if (nod >=  74 && nod <= 146) {"Discord"} else
+            if (nod >= 147 && nod <= 219) {"Confusion"} else
+            if (nod >= 220 && nod <= 292) {"Bureaucracy"} else
+            if (nod >= 293 && nod <= 365) {"The Aftermath"}
   
-  holyday <- if(nod == 5  && season == "Chaos"){"Mungday"} else
-             if(nod == 50 && season == "Chaos"){"Chaoflux"} else
-             if(nod == 5  && season == "Discord"){"Mojoday"} else
-             if(nod == 50 && season == "Discord"){"Discoflux"} else
-             if(nod == 5  && season == "Confusion"){"Syaday"} else
-             if(nod == 50 && season == "Confusion"){"Confuflux"} else
-             if(nod == 5  && season == "Bureaucracy"){"Zaraday"} else
-             if(nod == 50 && season == "Bureaucracy"){"Bureflux"} else
-             if(nod == 5  && season == "The Aftermath"){"Maladay"} else
-             if(nod == 50 && season == "The Aftermath"){"Afflux"} else {"nevermind"}
+  holyday <- if (nod == 5  && season == "Chaos") {"Mungday"} else
+             if (nod == 50 && season == "Chaos") {"Chaoflux"} else
+             if (nod == 5  && season == "Discord") {"Mojoday"} else
+             if (nod == 50 && season == "Discord") {"Discoflux"} else
+             if (nod == 5  && season == "Confusion") {"Syaday"} else
+             if (nod == 50 && season == "Confusion") {"Confuflux"} else
+             if (nod == 5  && season == "Bureaucracy") {"Zaraday"} else
+             if (nod == 50 && season == "Bureaucracy") {"Bureflux"} else
+             if (nod == 5  && season == "The Aftermath") {"Maladay"} else
+             if (nod == 50 && season == "The Aftermath") {"Afflux"} else {"nevermind"}
              
   
   
   # some things are just not meant to be defined (for now).
-  return(cat("Today is", paste0(if(holyday != "nevermind"){holyday} 
+  return(cat("Today is", paste0(if (holyday != "nevermind") {holyday} 
                                 else {as.character(year[nod])}, ", the"), 
              paste0(nod, "th Day of"), season, "in the YOLD", yold))
   }
@@ -58,3 +58,10 @@ ddate <- function(){
 
 nod <- as.numeric(Sys.Date() - as.Date("2016-01-01"))
 print(paste0("Today is ", year[nod]))
+
+
+  # Acoount for St. Tib's Day
+
+    # if 60th day of chaos is not Setting Orange then St. Tib's Day
+
+if (nod == 60 && season == "Chaos" && as.character(year[nod]) != "Setting Orange") {"It's St. Tib's Day!"} else {year[nod]}
