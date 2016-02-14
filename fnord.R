f3259331e273b3b9cd18d402ca7803d5a537e8fd
MarauderPixie <tobias.anton.85@gmail.com>
2016-02-14 23:36:22 +0100

    eaten by a grue

--- fnord.R
+++ fnord.R
@@ -3,7 +3,6 @@ fnord <- function(){
               "You said something...?",
               "It is pitch black.",
               "You are likely to be eaten by a grue.",
-              "If you can't see it",
               "Feeling dizzy?",
               "IF YOU DON'T SEE THE FNORD IT CAN'T EAT YOU",
               "Don't forget your towel.",
