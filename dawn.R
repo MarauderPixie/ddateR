60a7841af69310e93ef88456a5d075b30af1edbf
MarauderPixie <tobias.anton.85@gmail.com>
2016-02-14 13:14:10 +0100

    don't ask me

--- dawn.R
+++ dawn.R
@@ -1,23 +1,24 @@
-days <- factor(c("Sweetmorn", "Boomtime", "Pungenday", "Prickle-Prickle", "Setting Orange"))
-seasons <- factor(c("Chaos", "Discord", "Confusion", "Bureaucracy", "The Aftermath"))
-
-holydays <- data.frame("5th"  = c("Mungday", "Mojoday", "Syaday", "Zaraday", "Maladay"),
-                       "50th" = c("Chaoflux", "Discoflux", "Confuflux", "Bureflux", "Afflux"))
 ## ...?
-holydays.m <- matrix(holydays)
 
 ddate <- function(){
-  days     <- factor(c("Sweetmorn", "Boomtime", "Pungenday", "Prickle-Prickle", "Setting Orange"))
-  seasons  <- factor(c("Chaos", "Discord", "Confusion", "Bureaucracy", "The Aftermath"))
+  days     <- factor(c(1:5), labels = c("Sweetmorn", "Boomtime", "Pungenday", "Prickle-Prickle", "Setting Orange"))
+  seasons  <- factor(c(1:5), labels = c("Chaos", "Discord", "Confusion", "Bureaucracy", "The Aftermath"))
   
   holydays <- data.frame("5th"  = c("Mungday", "Mojoday", "Syaday", "Zaraday", "Maladay"),
                          "50th" = c("Chaoflux", "Discoflux", "Confuflux", "Bureflux", "Afflux"))
+  holydays.m <- matrix(holydays)
   
-  today <- as.list(unlist(strsplit(system("date",T)," ")))
-  names(today)<-c("weekday","month","day","time","timezone","year")
+  yold <- as.numeric(today$year) + 1166
   
-  YOLD <- as.numeric(today$year) + 1166
+  
+  today <- as.list(unlist(strsplit(system("date", T), " ")))
+  names(today) <- c("weekday", "day", "month", "time", "timezone", "year")
+  
+  
+  
   return(*object*)
   }
+
+*object* sth sth <- c("%day, %count'th Day of %season in the YOLD %yold")
 
 system(command = 'ddate', intern = T)
