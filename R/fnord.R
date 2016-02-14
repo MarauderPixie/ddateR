75e7d0de3df7200f79d1a60a9799c361ce58dd05
MarauderPixie <tobias.anton.85@gmail.com>
2016-02-15 00:43:35 +0100

    commit ALL the files!

---
+++ R/fnord.R
@@ -0,0 +1,31 @@
+fnord <- function(){
+  sermon <- c("RUN!",
+              "You said something...?",
+              "It is pitch black.",
+              "You are likely to be eaten by a grue.",
+              "Feeling dizzy?",
+              "IF YOU DON'T SEE THE FNORD IT CAN'T EAT YOU",
+              "Don't forget your towel.",
+              "Hail Eris.",
+              "stub",
+              "thud",
+              "Did you hear that?",
+              "fnord",
+              "Dull but sincere filler.",
+              "Tao Fa Tsu-Dan.",
+              "If the phone rings today, water it!",
+              "Reality is the original Rorschach.",
+              "Convictions cause convicts.",
+              "It's not you, it's determinism.",
+              "...",
+              "7. never write in pencil unless you are on a train or sick in bed.",
+              "Did you know you have a lopsided pineal gland?",
+              "Why did the chicken cross the road.",
+              "Yo mama's so... wait, that's not right.",
+              "Don't eat the yellow snow. Unless you're into that kind of thing, then go ahead.",
+              "00034")
+  
+  use <- sample(sermon, 1, replace = T)
+  
+  return(use)
+}
