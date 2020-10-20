822f9b274df2522ae017e4f68223d060832beae2
MarauderPixie <tobias.anton.85@gmail.com>
2020-10-20 11:35:29 +0200

    is it dead yet?

--- R/fnord.R
+++ R/fnord.R
@@ -13,12 +13,14 @@ fnord <- function(){
               "IF YOU DON'T SEE THE FNORD IT CAN'T EAT YOU",
               "Don't forget your towel.",
               "Hail Eris.",
-              "stub",
+              "All hail Discordia.",
               "thud",
               "Did you hear that?",
               "fnord",
               "Dull but sincere filler.",
               "Tao Fa Tsu-Dan.",
+              "Oh that guy? Man, he's got issues.",
+              "Better not mess with that lizard folk.",
               "If the phone rings today, water it!",
               "Reality is the original Rorschach.",
               "Convictions cause convicts.",
@@ -26,13 +28,15 @@ fnord <- function(){
               "...",
               "7. never write in pencil unless you are on a train or sick in bed.",
               "Did you know you have a lopsided pineal gland?",
-              "Why did the chicken cross the road.",
-              "Yo mama's so... wait, that's not right.",
-              "Don't eat the yellow snow. Unless you're into that kind of thing, then go ahead.",
+              "Why'd the chicken cross the road.",
+              # "Yo mama's so... wait, that's not right.",
+              "WATCH OUT!!",
+              "Don't eat the yellow snow. Unless you're into that kind of thing, then sure, go ahead, I guess.",
               "00034",
               "Or not.",
               "The uproar of one hand clapping.",
-              "ODD# II/2,xii;68Chs3136")
+              "ODD# II/2,xii;68Chs3136",
+              "Five is right out.")
 
   use <- sample(sermon, 1, replace = T)
 
